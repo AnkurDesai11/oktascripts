cda6d1aa656c18d27e480b5eb3d751544d5c2f0f
AnkurDesai11 <60006478+AnkurDesai11@users.noreply.github.com>
2022-05-18 01:28:45 +0530

    added mfa factors enrollment script

--- multithread_dynamichead/post_sms_voice_factors_gpid.py
+++ multithread_dynamichead/post_sms_voice_factors_gpid.py
@@ -21,7 +21,7 @@ selected_columns[0:0] = ["uid", "result"]
 ################################################################
 # CHANGE THE COLUMNS ASSIGNED BELOW TO OPERATION SPECIFIC VALUES
 ################################################################
-#selected_columns[len(selected_columns):len(selected_columns)] = ["Factors", "MFA_Voice_Number", "MFA_SMS_Number"]
+selected_columns[len(selected_columns):len(selected_columns)] = [ "MFA_SMS_Number", "MFA_SMS_Status", "MFA_Voice_Number", "MFA_Voice_Status"]
 
 base_url = input("Enter Okta tenant base url: ")
 api_token = input("Enter Okta api token: ")
@@ -59,15 +59,15 @@ else:
     if output_file_path == "user_factors_"+append_time+".csv":
         batch_list.to_csv(output_file_path, index=False)
 
-# execution_start = datetime.datetime.now()
-# print("Script execution started at:", execution_start)
-# logging.info('Script execution started at: %s',execution_start)
-# logging.info('Okta tenant base url: %s',base_url)
-# logging.info('Okta api token: %s',api_token)
-# logging.info('Input filepath: %s',input_file_path)
-# logging.info('Id to continue from: %s',id_to_continue_from)
-# logging.info('Output filepath: %s',output_file_path)
-# logging.info('Number of threads: %s',number_of_threads)
+execution_start = datetime.datetime.now()
+print("Script execution started at:", execution_start)
+logging.info('Script execution started at: %s',execution_start)
+logging.info('Okta tenant base url: %s',base_url)
+logging.info('Okta api token: %s',api_token)
+logging.info('Input filepath: %s',input_file_path)
+logging.info('Id to continue from: %s',id_to_continue_from)
+logging.info('Output filepath: %s',output_file_path)
+logging.info('Number of threads: %s',number_of_threads)
             
 #queue not iterable hence search in series, modify and then load in queue
 input_user_list_size = len(input_user_list)
@@ -75,110 +75,123 @@ shared_queue = queue.Queue(maxsize=0)
 [shared_queue.put(row) for row in input_user_list.to_dict('records')]
 input_user_list = None
 
-#print(list(shared_queue.queue))
-# thread_lock = threading.Lock()
+print(list(shared_queue.queue))
+thread_lock = threading.Lock()
 
-# urllib3.disable_warnings() #to suppress unverified https request warning
+urllib3.disable_warnings() #to suppress unverified https request warning
 
-# def update_progress(progress, message):
-#     print('\r[ {0}{1} ] {2}% {3}'.format('#' * int(progress/2), ' ' * int(50 - progress/2), progress, message),end="")
+def update_progress(progress, message):
+    print('\r[ {0}{1} ] {2}% {3}'.format('#' * int(progress/2), ' ' * int(50 - progress/2), progress, message),end="")
 
-# def worker_thread():
-#     user_details = {}
-#     rate_limit = 0
-#     rate_remaining = 0
-#     rate_reset = 0
-#     batch_list = pandas.DataFrame(columns = selected_columns)
-#     current_uid=""
-#     while not shared_queue.empty():
-#         try:
-#             uid = shared_queue.get()
-#             global total_processed
-#             thread_lock.acquire()
-#             total_processed += 1
-#             thread_lock.release()
-#             if uid is None or uid == "":
-#                 continue
-#             uid = '0'*int(8-len(str(uid)))+str(uid) if int(8-len(str(uid))) > 0 else str(uid)
-#             current_uid = uid
-#             uid="\""+uid+"\""
-#             endpoint = "{}/api/v1/users?search=profile.AD_LDAP_Mapper+eq+{}+or+profile.idx_Uid+eq+{}+or+profile.AD_SAMAccountName+eq+{}".format(base_url,uid,uid,uid)
-#             response = requests.get(endpoint, headers=headers, verify = False)
-#             limit = response.headers['x-rate-limit-limit']
-#             remain = response.headers['x-rate-limit-remaining']
-#             reset = response.headers['x-rate-limit-reset']
-#             if response.status_code==200:
-#                 if(len(response.json()) != 0):
-#                     current_user_details = pandas.json_normalize(response.json(), errors='ignore').reindex(columns=selected_columns).fillna('')
-#                     current_user_details.at[0, 'uid'] = current_uid
-#                     current_user_details.at[0, 'result'] = "Found"
-#                     endpoint = "{}/api/v1/users/{}/factors".format(base_url,current_user_details["id"].values[0])
-#                     response = requests.get(endpoint, headers=headers, verify = False)
-#                     if response.status_code==200:
-#                         if(len(response.json()) == 0):
-#                             current_user_details.at[0, 'result'] = "MFA not found"
-#                     else:
-#                         current_user_details.at[0, 'result'] = "Unexpected response from factors API"
+def worker_thread():
+    user_details = {}
+    rate_limit = 0
+    rate_remaining = 0
+    rate_reset = 0
+    batch_list = pandas.DataFrame(columns = selected_columns)
+    current_uid=""
+    while not shared_queue.empty():
+        try:
+            current_user_details = shared_queue.get()
+            global total_processed
+            thread_lock.acquire()
+            total_processed += 1
+            thread_lock.release()
+            uid = current_user_details["uid"]
+            sms_factor = current_user_details["sms"]
+            voice_factor = current_user_details["voice"]
+            if uid is None or uid == "":
+                continue
+            uid = '0'*int(8-len(str(uid)))+str(uid) if int(8-len(str(uid))) > 0 else str(uid)
+            current_uid = uid
+            uid="\""+uid+"\""
+            endpoint = "{}/api/v1/users?search=profile.AD_LDAP_Mapper+eq+{}+or+profile.idx_Uid+eq+{}+or+profile.AD_SAMAccountName+eq+{}".format(base_url,uid,uid,uid)
+            response = requests.get(endpoint, headers=headers, verify = False)
+            limit = response.headers['x-rate-limit-limit']
+            remain = response.headers['x-rate-limit-remaining']
+            reset = response.headers['x-rate-limit-reset']
+            if response.status_code==200:
+                if(len(response.json()) != 0):
+                    current_user_details = pandas.json_normalize(response.json(), errors='ignore').reindex(columns=selected_columns).fillna('')
+                    current_user_details.at[0, 'uid'] = current_uid
+                    current_user_details.at[0, 'result'] = "Found"
+                    current_user_details.at[0, 'MFA_SMS_Number'] = sms_factor
+                    current_user_details.at[0, 'MFA_Voice_Number'] = voice_factor
+                    endpoint1 = "{}/api/v1/users/{}/factors?activate=true".format(base_url,current_user_details["id"].values[0])
+                    body1 = { "factorType": "sms", "provider": "OKTA", "profile": { "phoneNumber": current_user_details["MFA_SMS_Number"].values[0] } }
+                    response1 = requests.post(endpoint1, data=body1, headers=headers, verify = False)
+                    if response1.status_code==200:
+                        current_user_details.at[0, 'MFA_SMS_Status'] = response1.json()["status"]
+                    else:
+                        current_user_details.at[0, 'MFA_SMS_Status'] = response1.json()["errorCauses"]["errorSummary"] if "errorSummary" in response1.json()["errorCauses"] else response1.json()["errorSummary"]
+                    
+                    endpoint2 = "{}/api/v1/users/{}/factors?activate=true".format(base_url,current_user_details["id"].values[0])
+                    body2 = { "factorType": "call", "provider": "OKTA", "profile": { "phoneNumber": current_user_details["MFA_Voice_Number"].values[0] } }
+                    response2 = requests.post(endpoint2, headers=headers, verify = False)
+                    if response1.status_code==200:
+                        current_user_details.at[0, 'MFA_SMS_Status'] = response1.json()["status"]
+                    else:
+                        current_user_details.at[0, 'MFA_SMS_Status'] = response1.json()["errorCauses"]["errorSummary"] if "errorSummary" in response1.json()["errorCauses"] else response1.json()["errorSummary"]
 
-#                     batch_list = pandas.concat([batch_list,current_user_details], ignore_index=True)
+                    batch_list = pandas.concat([batch_list,current_user_details], ignore_index=True)
 
-#                 else:
-#                     current_user_details = pandas.DataFrame(columns = selected_columns)
-#                     current_user_details.at[0, 'uid'] = current_uid
-#                     current_user_details.at[0, 'result'] = "User not found"
-#                     batch_list = pandas.concat([batch_list, current_user_details], ignore_index=True)
-#             else:
-#                 current_user_details = pandas.DataFrame(columns = selected_columns)
-#                 current_user_details.at[0, 'uid'] = current_uid
-#                 current_user_details.at[0, 'result'] = "Unexpected response from users API"
-#                 batch_list = pandas.concat([batch_list, current_user_details], ignore_index=True)
+                else:
+                    current_user_details = pandas.DataFrame(columns = selected_columns)
+                    current_user_details.at[0, 'uid'] = current_uid
+                    current_user_details.at[0, 'result'] = "User not found"
+                    batch_list = pandas.concat([batch_list, current_user_details], ignore_index=True)
+            else:
+                current_user_details = pandas.DataFrame(columns = selected_columns)
+                current_user_details.at[0, 'uid'] = current_uid
+                current_user_details.at[0, 'result'] = "Unexpected response from users API"
+                batch_list = pandas.concat([batch_list, current_user_details], ignore_index=True)
 
-#             if len(batch_list.index) >= 50:
-#                 thread_lock.acquire()
-#                 try:
-#                     batch_list.to_csv(output_file_path, index=False, header=False, mode='a')
-#                 except Exception as e:
-#                     logging.warning('Error while saving to file: %s',e)
-#                     logging.warning('DF - %s',batch_list.to_string())
-#                     logging.warning('List of failed input ids: %s',batch_list['uid'].tolist())
-#                     print("Error while saving to file: ",e)
-#                     print(batch_list.to_string())
-#                     print(batch_list['uid'].tolist())
-#                 thread_lock.release()
-#                 batch_list = batch_list[0:0]
-#                 update_progress( int((total_processed / input_user_list_size)*100) , "Users written to file : "+str(total_processed)+"              " )
-#             else:
-#                 update_progress( int((total_processed / input_user_list_size)*100) , "Users processed currently : "+str(total_processed)+"          " )
+            if len(batch_list.index) >= 50:
+                thread_lock.acquire()
+                try:
+                    batch_list.to_csv(output_file_path, index=False, header=False, mode='a')
+                except Exception as e:
+                    logging.warning('Error while saving to file: %s',e)
+                    logging.warning('DF - %s',batch_list.to_string())
+                    logging.warning('List of failed input ids: %s',batch_list['uid'].tolist())
+                    print("Error while saving to file: ",e)
+                    print(batch_list.to_string())
+                    print(batch_list['uid'].tolist())
+                thread_lock.release()
+                batch_list = batch_list[0:0]
+                update_progress( int((total_processed / input_user_list_size)*100) , "Users written to file : "+str(total_processed)+"              " )
+            else:
+                update_progress( int((total_processed / input_user_list_size)*100) , "Users processed currently : "+str(total_processed)+"          " )
 
-#             if int(limit)-int(remain) > int(limit)*0.4:
-#                 update_progress( int((total_processed / input_user_list_size)*100) , "Waiting for rate limit: "+
-#                                 str( abs( int(reset) - int(time.time()) ) )+"s, "+
-#                                 str(total_processed)+" done " )
-#                 time.sleep(abs( int(reset) - int(time.time()) ) )
+            if int(limit)-int(remain) > int(limit)*0.4:
+                update_progress( int((total_processed / input_user_list_size)*100) , "Waiting for rate limit: "+
+                                str( abs( int(reset) - int(time.time()) ) )+"s, "+
+                                str(total_processed)+" done " )
+                time.sleep(abs( int(reset) - int(time.time()) ) )
 
-#             shared_queue.task_done()
+            shared_queue.task_done()
 
-#         except Exception as e:
-#             current_user_details = pandas.DataFrame(columns = selected_columns)
-#             current_user_details.at[0, 'uid'] = current_uid
-#             current_user_details.at[0, 'result'] = "Runtime Error"
-#             batch_list = pandas.concat([batch_list, current_user_details], ignore_index=True)
-#             shared_queue.task_done()
-#             print("Runtime Error: ",e)
+        except Exception as e:
+            current_user_details = pandas.DataFrame(columns = selected_columns)
+            current_user_details.at[0, 'uid'] = current_uid
+            current_user_details.at[0, 'result'] = "Runtime Error"
+            batch_list = pandas.concat([batch_list, current_user_details], ignore_index=True)
+            shared_queue.task_done()
+            print("Runtime Error: ",e)
 
-#     if len(batch_list.index) != 0:
-#         thread_lock.acquire()
-#         batch_list.to_csv(output_file_path, index=False, header=False, mode='a')
-#         thread_lock.release()
+    if len(batch_list.index) != 0:
+        thread_lock.acquire()
+        batch_list.to_csv(output_file_path, index=False, header=False, mode='a')
+        thread_lock.release()
 
-# for i in range(number_of_threads):
-#     t = threading.Thread(target = worker_thread)
-#     t.start()
+for i in range(number_of_threads):
+    t = threading.Thread(target = worker_thread)
+    t.start()
 
-# shared_queue.join()
+shared_queue.join()
 
-# execution_end = datetime.datetime.now()
-# print("\nScript execution completed at:", execution_end)
-# print("Total time taken for script execution:", (execution_end - execution_start))
-# logging.info('Script execution completed at: %s',execution_end)
-# logging.info('Total time taken for script execution: %s',(execution_end - execution_start))
+execution_end = datetime.datetime.now()
+print("\nScript execution completed at:", execution_end)
+print("Total time taken for script execution:", (execution_end - execution_start))
+logging.info('Script execution completed at: %s',execution_end)
+logging.info('Total time taken for script execution: %s',(execution_end - execution_start))
